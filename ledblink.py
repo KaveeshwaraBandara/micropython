a6c9543f2290a3410d4c33806e07923fefd39e7e
Imanchana Kaveeshwara Bandara <bandaragamik.22@uom.lk>
2025-01-08 23:32:09 +0530

    write some basic codes

---
+++ ledblink.py
@@ -0,0 +1,15 @@
+import machine
+import time
+
+led = machine.Pin(2, machine.Pin.OUT)
+
+counter = 0
+
+while (counter < 5):
+    led.on()   "also we can use led.value(1)"
+    time.sleep(0.5)
+    led.off()
+    time.sleep(0.5)
+    counter+=1
+    
+print("Blinking led is done")
---
+++ ledblinkfunction.py
@@ -0,0 +1,15 @@
+import machine
+import time
+
+led = machine.Pin(2, machine.Pin.OUT)
+
+counter = 0
+
+def blink_LED_ntimes(num, t_on, t_off, msg):
+    while (counter < num):
+    led.on()   "also we can use led.value(1)"
+    time.sleep(t_on)
+    led.off()
+    time.sleep(t_off)
+    counter+=1
+    print(msg)
